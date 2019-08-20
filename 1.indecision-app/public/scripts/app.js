b01dbe262b705071dac1530ac1a59d4e04ba9dec
Marcia Tobias <marciaatobiass@gmail.com>
2019-08-20 11:27:59 +0100

    Creating template two

--- 1.indecision-app/public/scripts/app.js
+++ 1.indecision-app/public/scripts/app.js
@@ -3,12 +3,66 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
+// wrap div
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Marcia Tobias'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info!'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+// Create a templateTwo var JSX expression
+// div
+//  h1 -> your name
+// p -> Age: 26
+// p -> Location: 
+// Render templateTwo instead of template
+
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Marcia Tobias'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 36'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Ireland'
+    )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
+
+// ReactDOM.render(template, appRoot);
--- 1.indecision-app/src/app.js
+++ 1.indecision-app/src/app.js
@@ -1,8 +1,36 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
+// wrap div
+var template = (
+    <div>
+        <h1>Marcia Tobias</h1>
+        <p>This is some info!</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+// Create a templateTwo var JSX expression
+// div
+//  h1 -> your name
+// p -> Age: 26
+// p -> Location: 
+// Render templateTwo instead of template
+
+
+var templateTwo = (
+    <div>
+        <h1>Marcia Tobias</h1>
+        <p>Age: 36</p>
+        <p>Location: Ireland</p>
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
+
+// ReactDOM.render(template, appRoot);
